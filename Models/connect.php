accd6774a3e862b4ec767af3a07bbc666c9284d2
jgonzalezromer <jgonzalezromero@danielcastelao.org>
2025-03-18 18:37:31 +0100

    Archivo connect.php terminado

--- Models/connect.php
+++ Models/connect.php
@@ -1,20 +1,29 @@
 <?php
 
-// Credenciais para o inicio de sesion en mysql
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "catalogo";
-$port= 3308;
+// Función para a conexión coa base de datos
+function ConexionDB(){
+    // Credenciais para o inicio de sesion en mysql
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "catalogo";
+    $port= 3308;
 
-// Creamos a conexión con mysql
-$conn = new mysqli($servername, $username, $password,$database,$port);
+    // Creamos a conexión con mysql
+    $conn = new mysqli($servername, $username, $password,$database,$port);
 
-// Comprobar se hai un erro ao conectar 
-if ($conn->connect_error) {
-    // Con die detemos o script
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "<h1>Conexión establecida</h1>";
+    // Comprobar se hai un erro ao conectar 
+    if ($conn->connect_error) { 
+        // Con die detemos o script
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn; // Devolvemos a conexión
+}
+
+// Función para cerrar a conexión coa base de datos
+function DesconexionDB($conn){
+    // Cerramos sesion con close()
+    $conn->close();
 }
 ?>
