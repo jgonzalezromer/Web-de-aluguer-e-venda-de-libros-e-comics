4a2b378a79632c7189a1039715a8e5ca99ca1a4f
jgonzalezromero <jgonzalezromero@danielcastelao.org>
2025-03-18 01:16:33 +0100

    Creación de archivos primarios

---
+++ menu_usuarios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE hyml>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menú para usuarios</title>
+    </head>
+    <body>
+        <h1>Benvido</h1>
+    </body>
+</html>
